46ec07a4974cdd3592d63877291d6e8c5b351c26
errorrik <errorrik@gmail.com>
2019-05-30 17:03:25 +0800

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import san from 'san';
+import {Link} from 'san-router';
+
+export default san.defineComponent({
+    components: {
+        'x-link': Link
+    },
+
+    template: `
+        <footer>
+          <div class="container">
+            <x-link to="/" class="logo-font">conduit</x-link>
+            <span class="attribution">
+              An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+            </span>
+          </div>
+        </footer>
+    `
+})
