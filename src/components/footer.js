5f2bb90be91aedbe0a66318e9370780a38409f3b
errorrik <errorrik@gmail.com>
2019-05-31 18:03:04 +0800

    remove root

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,13 +7,11 @@ export default san.defineComponent({
     },
 
     template: `
-        <footer>
-          <div class="container">
-            <x-link to="/" class="logo-font">conduit</x-link>
-            <span class="attribution">
-              An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
-            </span>
-          </div>
-        </footer>
+      <div class="container">
+        <x-link to="/" class="logo-font">conduit</x-link>
+        <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
     `
 })
