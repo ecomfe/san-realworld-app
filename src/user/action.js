63e8ee3366c20d3d547319cf5ee2ee90e06bac41
errorrik <errorrik@gmail.com>
2019-06-12 15:32:00 +0800

    add actions

--- src/user/action.js
+++ src/user/action.js
@@ -1,5 +1,6 @@
 import { store } from 'san-store';
 import { updateBuilder } from 'san-update';
+import axios from 'axios';
 import service from './service';
 import jwt from '../common/jwt';
 import { Types as CommonActionTypes } from '../common/action';
@@ -7,9 +8,11 @@ import { Types as CommonActionTypes } from '../common/action';
 
 export const Types = {
     LOGIN: 'userLogin',
+    GET: 'userGet',
     REGISTER: 'userRegister',
     SET_AUTH: 'userSetAuth',
-    PURGE_AUTH: 'userPurgeAuth'
+    PURGE_AUTH: 'userPurgeAuth',
+    USER_UPDATE: 'userUpdate'
 };
 
 store.addAction(Types.LOGIN, function (payload, {dispatch}) {
@@ -24,6 +27,26 @@ store.addAction(Types.LOGIN, function (payload, {dispatch}) {
             
         }
     );
+});
+
+
+store.addAction(Types.GET, function (payload, {getState, dispatch}) {
+    if (getState('user')) {
+        return;
+    }
+
+    let token = jwt.getToken();
+    if (token) {
+        setRequestHeaderToken(token);
+        return service.get().then(({data}) => {
+            if (data.errors) {
+                store.dispatch(CommonActionTypes.ERRORS_SET, data.errors);
+            }
+            else {
+                store.dispatch(Types.SET_AUTH, data.user);
+            }
+        });
+    }
 });
 
 store.addAction(Types.REGISTER, function (payload, {dispatch}) {
@@ -42,6 +65,7 @@ store.addAction(Types.REGISTER, function (payload, {dispatch}) {
 
 store.addAction(Types.SET_AUTH, function (user, {dispatch}) {
     jwt.setToken(user.token);
+    setRequestHeaderToken(user.token);
     return updateBuilder()
         .set('user', user)
         .set('isAuthenticated', true);
@@ -49,7 +73,40 @@ store.addAction(Types.SET_AUTH, function (user, {dispatch}) {
 
 store.addAction(Types.PURGE_AUTH, function (user, {dispatch}) {
     jwt.clearToken();
+    delete axios.defaults.headers.common['Authorization'];
     return updateBuilder()
-        .set('user', null)
+        .set('user', {})
         .set('isAuthenticated', false);
 });
+
+store.addAction(Types.USER_UPDATE, function (payload, {dispatch}) {
+    const { email, username, password, image, bio } = payload;
+    const user = {
+        email,
+        username,
+        bio,
+        image
+    };
+
+    if (password) {
+        user.password = password;
+    }
+
+    return service.update(user).then(
+        ({data}) => {
+            if (data.errors) {
+                dispatch(CommonActionTypes.ERRORS_SET, data.errors);
+            }
+            else {
+                dispatch(Types.SET_AUTH, data.user);
+            }
+            
+        }
+    );
+});
+
+function setRequestHeaderToken(token) {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = `Token ${token}`;
+    }
+}
