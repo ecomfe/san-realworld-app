0d62bf4c9b7c3ed9bc1ce224f85908affea606a5
errorrik <errorrik@gmail.com>
2019-07-03 11:45:37 +0800

    use a to replace Link component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,9 @@
 import san from 'san';
-import {Link} from 'san-router';
 
 export default san.defineComponent({
-    components: {
-        'x-link': Link
-    },
-
     template: `
       <div class="container">
-        <x-link to="/" class="logo-font">conduit</x-link>
+        <a href="#/" class="logo-font">conduit</a>
         <span class="attribution">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
         </span>
