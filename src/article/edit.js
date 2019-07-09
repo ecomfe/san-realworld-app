12534478842df51f87fb08c6c3639252ff63c85a
errorrik <errorrik@gmail.com>
2019-07-09 12:39:15 +0800

    onsubmit

--- src/article/edit.js
+++ src/article/edit.js
@@ -7,9 +7,7 @@ import ErrorsView from '../common/components/errors';
 export default connect.san(
     {
         article: 'article',
-        user: 'user',
-        isAuthenticated: 'isAuthenticated',
-        errors: 'errors'
+        isAuthenticated: 'isAuthenticated'
     },
     {
         reset: ActionTypes.RESET,
@@ -30,7 +28,7 @@ export default connect.san(
             <div class="row">
               <div class="col-md-10 offset-md-1 col-xs-12">
                 <x-errors />
-                <form onsubmit="return false;">
+                <form on-submit="onPublish">
                   <fieldset disabled="{{inProgress}}">
                     <fieldset class="form-group">
                       <input type="text" class="form-control form-control-lg" value="{=article.title=}" placeholder="Article Title">
@@ -45,7 +43,7 @@ export default connect.san(
                       </textarea>
                     </fieldset>
                     <fieldset class="form-group">
-                      <input type="text" class="form-control" placeholder="Enter tags" value="{=tagInput=}" on-keyup="addTag($event)">
+                      <input type="text" class="form-control" placeholder="Enter tags" value="{=tagInput=}" on-keypress="addTag">
                       <div class="tag-list">
                         <span class="tag-default tag-pill" s-for="tag in article.tagList">
                           <i class="ion-close-round" on-click="removeTag(tag)"></i>
@@ -54,7 +52,7 @@ export default connect.san(
                       </div>
                     </fieldset>
                   </fieldset>
-                  <button disabled="{{inProgress}}" class="btn btn-lg pull-xs-right btn-primary" on-click="onPublish()">
+                  <button disabled="{{inProgress}}" class="btn btn-lg pull-xs-right btn-primary">
                     Publish Article
                   </button>
                 </form>
@@ -76,14 +74,15 @@ export default connect.san(
         this.actions.reset();
     },
 
-    onPublish() {
+    onPublish(e) {
+        e.preventDefault();
         this.data.set('inProgress', true);
 
         let slug = this.data.get('route.query.slug');
         this.actions[slug ? 'edit' : 'add'](this.data.get('article'))
             .then(data => {
                 if (data.errors) {
-                    this.data.set('inProgress', false);
+                    this.data.set('inProgress', null);
                     return;
                 }
 
@@ -93,6 +92,7 @@ export default connect.san(
 
     addTag(e) {
         if ((e.which || e.keyCode) === 13) {
+            e.preventDefault();
             let tagInput = this.data.get('tagInput');
 
             if (tagInput) {
