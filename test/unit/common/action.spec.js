7f1e048e4d52f3543ccbcd5e5b4c462bc72d325b
errorrik <errorrik@gmail.com>
2019-07-25 22:26:00 +0800

    add common action spec

---
+++ test/unit/common/action.spec.js
@@ -0,0 +1,15 @@
+import { store } from 'san-store';
+import {Types} from '../../../src/common/action';
+
+describe('Common Action', () => {
+  it('set errors and clear errors', () => {
+    store.dispatch(Types.ERRORS_SET, {title: 'one'});
+
+    let errors = store.getState('errors');
+    expect(errors.length).toBe(1);
+    expect(errors[0]).toBe('title one');
+
+    store.dispatch(Types.ERRORS_CLEAR);
+    expect(store.getState('errors') == null).toBeTruthy();
+  });
+});
