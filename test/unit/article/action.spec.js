7337797582ecdda012ba4474007171c4e6bb2458
errorrik <errorrik@gmail.com>
2019-07-26 14:10:18 +0800

    update expect, use config

--- test/unit/article/action.spec.js
+++ test/unit/article/action.spec.js
@@ -1,6 +1,8 @@
 import { store } from 'san-store';
 import { Types } from '../../../src/article/action';
 
+import Conf from '../../../src/common/config';
+
 describe('Acticle Action', () => {
   it('fetch list', done => {
     let fetchPromise = store.dispatch(Types.FETCH);
@@ -10,7 +12,7 @@ describe('Acticle Action', () => {
     fetchPromise.then(() => {
         expect(store.getState('articlesLoading')).toBeFalsy();
         expect(store.getState('articleCount')).toBe(500);
-        expect(store.getState('articlePageCount')).toBeGreaterThan(0);
+        expect(store.getState('articlePageCount') >= store.getState('articleCount') / Conf.PAGE_SIZE).toBeTruthy();
         expect(store.getState('articles') instanceof Array).toBeTruthy();
 
         done();
